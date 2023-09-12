c389e8cdd488593713214ed6c5743a5078f6408d
erikvella <vella.erik1@gmail.com>
2023-09-12 17:01:56 +0100

    [add] true false

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
  const listaMail = ['primo@gmail.com' , 'secondo@gmail.com' , 'terzo@gmail.com'];
  const sendBtn = document.getElementById('send');
- 
+ let datiValidi = false;
 
 
 // metodo lungo funzionante
@@ -33,12 +33,19 @@ sendBtn.addEventListener('click' , function(){
   for(let i = 0 ; i < listaMail.length ; i++){
 
     if(userMail === listaMail[i]){
+      datiValidi = true;
       console.log('mail valida');
-      document.getElementById('output-mail').innerHTML = 'Mail valida!' ;
+      
     }else{
+      datiValidi = false;
       console.log('mail non valida');
-      document.getElementById('output-mail').innerHTML = 'Mail non valida!' ;
+     
     }
+   }
+   if(datiValidi = true){
+    document.getElementById('output-mail').innerHTML = 'Mail valida!' ;
+   }else{
+    document.getElementById('output-mail').innerHTML = 'Mail non valida!' ;
    }
 })
  
@@ -56,16 +63,16 @@ const nPc =  Math.floor(Math.random() * (max - min + 1) + min);
 lanciaDadi.addEventListener('click' , function(){
   if(nUtente < nPc){
     console.log('Ha vinto il PC!');
-  document.getElementById('output-dice').innerHTML = 'Ha vinto il PC poichè ha effettutato un ' + ${nPc} + 'mentre il giocatore ha effettuato un ' + ${nUtente};
+  document.getElementById('output-dice').innerHTML = `Ha vinto il PC poichè ha effettutato un ${nPc} mentre il giocatore ha effettuato un ${nUtente}`;
   }
   
   if(nUtente > nPc){
     console.log('Ha vinto il giocatore!');
-    document.getElementById('output-dice').innerHTML = 'Ha vinto il giocatore poichè ha effettutato un ' + ${nUtente} + 'mentre il PC ha effettuato un ' + ${nPc};
+    document.getElementById('output-dice').innerHTML = `Ha vinto il giocatore poichè ha effettutato un ${nUtente} mentre il PC ha effettuato un ${nPc}`;
   }
 
   if(nUtente === nPc){
     console.log('Parità!')
-    document.getElementById('output-dice').innerHTML = 'Esito di parità poichè il giocatore ha effettuato un ' + ${nUtente} + 'mentre il PC ha effettuato un ' + ${nPc};
+    document.getElementById('output-dice').innerHTML = `Esito di parità poichè il giocatore ha effettuato un  ${nUtente} mentre il PC ha effettuato un ${nPc}`;
   }
   })
