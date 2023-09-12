fd1a0a13088b86ce36f9b2820a8d9452c8947d17
erikvella <vella.erik1@gmail.com>
2023-09-12 14:10:39 +0100

    [init] email

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// esercizio dell'email 
+
+const listaMail = ['primo@gmail.com' , 'secondo@gmail.com' , 'terzo@gmail.com'];
+
+const userMail = prompt('Inserire email');
+const validMail = listaMail[i];
+
+let message;
+
+if(userMail === validMail){
+  console.log('mail valida')
+}
