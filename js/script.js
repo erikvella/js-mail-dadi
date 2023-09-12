89ec0067f339d2ea42a587724314f28a12452d6d
erikvella <vella.erik1@gmail.com>
2023-09-12 14:38:05 +0100

    [fix] mail control

--- js/script.js
+++ js/script.js
@@ -5,12 +5,31 @@ const listaMail = ['primo@gmail.com' , 'secondo@gmail.com' , 'terzo@gmail.com'];
 const userMail = prompt('Inserire email');
 
 
+// metodo lungo funzionante
+
+// for(let i = 0 ; i < listaMail.length ; i++){
+  
+//   if(userMail === 'primo@gmail.com'){
+//     console.log('mail valida');
+
+//   }else if (userMail === 'secondo@gmail.com'){
+//     console.log('mail valida');
+
+//  }else if (userMail === 'terzo@gmail.com'){
+//   console.log('mail valida');
+//  }else{
+//   console.log('mail non valida')
+//  }
+// }
+
+// metodo compatto
 
 for(let i = 0 ; i < listaMail.length ; i++){
-  let validMail = [i];
-  if(userMail === validMail){
+  if(userMail === listaMail[i]){
     console.log('mail valida');
   }else{
     console.log('mail non valida');
   }
+  
+
 }
